d02f3a0c3c1f7539c51904002e03c56032ba57c9
sailesh818 <sailesh7019@gmail.com>
2025-05-09 13:54:40 +0545

    navigation bar added

---
+++ lib/home/page/home1_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class Home1Page extends StatefulWidget {
+  const Home1Page({super.key});
+
+  @override
+  State<Home1Page> createState() => _Home1PageState();
+}
+
+class _Home1PageState extends State<Home1Page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('')),
+      body: Column(
+        children: [
+          GridView.builder(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            //itemCount: ,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              childAspectRatio: 2 / 3,
+            ),
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => FoodsDetailsPage(food: food),
+                    ),
+                  );
+                },
+                child: Container(),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/home/page/home_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int selectedIndex = 0;
+
+  List<Widget> pages = [
+    Center(child: Text('Homepage')),
+    Center(child: Text('Profilepage')),
+    Center(child: Text('Searchpage')),
+  ];
+  void onItem(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('')),
+      body: pages[selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedIndex,
+        onTap: onItem,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+        ],
+      ),
+    );
+  }
+}
